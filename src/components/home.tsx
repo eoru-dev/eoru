b8d79f7cdc29d50a85c6dc3481b799c6a9499791
eoru-dev <george@eoru.dev>
2025-07-09 01:10:42 +1000

    title section

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -7,62 +7,78 @@ export default function Home() {
     <div className="relative flex min-h-screen w-screen items-center justify-center p-2">
       <Background />
 
-      <div className="relative h-[700px] w-full max-w-3xl">
-        <div className="clip-slant-right bg-primary-500 text-primary-100 absolute bottom-full flex w-1/4 items-center py-0.5 px-4 text-xs font-semibold select-none">
-          eoru.dev
+      <div
+        className={clsx(
+          "relative flex h-[700px] w-full max-w-3xl flex-col justify-between gap-2 overflow-hidden",
+          "shadow-primary-100 shadow-[0_0_10px]",
+          "from-primary-300/90 to-primary-400/90 bg-radial to-70%",
+        )}
+      >
+        <div className="from-primary-500 via-primary-500 to-primary-100 h-7 bg-gradient-to-b via-65%">
+          <div className="shadow-primary-100 h-full w-full opacity-20 shadow-[0_0_2px_2px,0_0_4px_4px,0_0_8px_8px,0_0_16px_16px,0_0_32px_32px,0_0_64px_64px]" />
         </div>
-        <div className="flex h-full flex-col justify-between gap-2 overflow-hidden">
-          <div className="from-primary-300 to-primary-400 absolute -z-1 h-full w-full bg-radial to-70% opacity-90" />
 
-          <div className="from-primary-500 via-primary-500 to-primary-100 mb-2 h-7 bg-gradient-to-b via-65%">
-            <div className="shadow-primary-100 h-full w-full opacity-20 shadow-[0_0_2px_2px,0_0_4px_4px,0_0_8px_8px,0_0_16px_16px,0_0_32px_32px,0_0_64px_64px]" />
-          </div>
-
-          {/* 
-          <div className="flex w-full flex-col gap-4 p-6">
-            <div className="flex items-center gap-4">
-              <Image
-                src="/cat.gif"
-                width="0"
-                height="0"
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                className="h-auto w-1/4 object-contain"
-                alt="cat profile image"
-                unoptimized
-                priority
-              />
-              <div>
-                <div className="font-pixel text-5xl font-semibold">
-                  George Zhu
-                </div>
-                <div className="text-3xl text-zinc-400">Software Engineer</div>
+        <div className="flex h-full w-full flex-col gap-4 p-6">
+          <div className="relative flex gap-4">
+            <div
+              className={clsx(
+                "bg-primary-500/60 absolute w-full",
+                "h-18 sm:h-24",
+              )}
+            />
+            <Image
+              src="/cat.gif"
+              width="0"
+              height="0"
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className={clsx(
+                "relative h-auto w-1/3 shrink-0 object-contain",
+                "hidden sm:block",
+              )}
+              alt="cat profile image"
+              unoptimized
+              priority
+            />
+            <div className="w-full">
+              <div
+                className={clsx(
+                  "font-pixel relative px-4 text-shadow-[0_6px_0_var(--color-primary-100)]",
+                  "text-7xl sm:text-8xl",
+                )}
+              >
+                George Zhu
               </div>
-            </div>
-
-            <div className="flex gap-2">
-              <div className="w-full bg-zinc-900/25 p-2">
-                <div className="text-2xl font-semibold">about</div>
-                I'm a software engineer interested in web development and game
-                programming.
-              </div>
-              <div className="w-full p-2">
-                <div className="text-2xl font-semibold">skills</div>
-                🕹️ Game Development Unreal Engine, AI Behavior Trees, Procedural
-                Generation 🌐 Web Development Full-stack with Next.js, TRPC,
-                Prisma, PostgreSQL UI/UX with React, Material-UI, TailwindCSS 🧠
-                Problem Solving & Optimization Backend route tuning, scalable
-                data handling, performance engineering
+              <div className="text-primary-50 px-4 text-2xl font-semibold">
+                Software Developer
               </div>
             </div>
           </div>
+
+          <div className="flex h-full gap-2">
+            <div className="w-full bg-zinc-900/25 p-2">
+              <div className="text-2xl font-semibold">about</div>
+              I'm a software engineer interested in web development and game
+              programming.
+            </div>
+            <div className="w-full p-2">
+              <div className="text-2xl font-semibold">skills</div>
+              🕹️ Game Development Unreal Engine, AI Behavior Trees, Procedural
+              Generation 🌐 Web Development Full-stack with Next.js, TRPC,
+              Prisma, PostgreSQL UI/UX with React, Material-UI, TailwindCSS 🧠
+              Problem Solving & Optimization Backend route tuning, scalable data
+              handling, performance engineering
+            </div>
+          </div>
+
+          <hr className="bg-primary-300/80 my-2 h-2 border-t-0" />
 
           <div className="flex w-full items-center justify-center text-xl">
             Home | Experience | Projects | Education
-          </div> */}
-
-          <div className="from-primary-500 via-primary-500 to-primary-100 mt-2 h-7 bg-gradient-to-t via-65%">
-            <div className="shadow-primary-100 h-full w-full opacity-20 shadow-[0_0_2px_2px,0_0_4px_4px,0_0_8px_8px,0_0_16px_16px,0_0_32px_32px,0_0_64px_64px]" />
           </div>
+        </div>
+
+        <div className="from-primary-500 via-primary-500 to-primary-100 h-7 bg-gradient-to-t via-65%">
+          <div className="shadow-primary-100 h-full w-full opacity-20 shadow-[0_0_2px_2px,0_0_4px_4px,0_0_8px_8px,0_0_16px_16px,0_0_32px_32px,0_0_64px_64px]" />
         </div>
       </div>
     </div>
